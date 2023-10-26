6068a2cc2bd91e657fecad80438541e4edd7c555
Christine Anthony <2christineanthony@gmail.com>
2023-10-26 17:16:34 -0400

    added basic validation functions; added project proposal to documents directory

--- data/validation.js
+++ data/validation.js
@@ -1,5 +1,65 @@
-// string
+import { ObjectId } from "mongodb";
 
-// object
-//testing a change
-//testing a change
+const helperFunctions = {
+  checkId(id, varName) {
+    if (!id) throw `Error: You must provide a ${varName}`;
+    if (typeof id !== "string") throw `Error:${varName} must be a string`;
+    id = id.trim();
+    if (id.length === 0)
+      throw `Error: ${varName} cannot be an empty string or just spaces`;
+    if (!ObjectId.isValid(id)) throw `Error: ${varName} invalid object ID`;
+    return id;
+  },
+
+  checkString(strVal, varName) {
+    if (!strVal) throw `Error: You must supply a ${varName}!`;
+    if (typeof strVal !== "string") throw `Error: ${varName} must be a string!`;
+    strVal = strVal.trim();
+    if (strVal.length === 0)
+      throw `Error: ${varName} cannot be an empty string or string with just spaces`;
+    if (!isNaN(strVal))
+      throw `Error: ${strVal} is not a valid value for ${varName} as it only contains digits`;
+    return strVal;
+  },
+
+  checkStringArray(arr, varName) {
+    //We will allow an empty array for this,
+    //if it's not empty, we will make sure all tags are strings
+    if (!arr || !Array.isArray(arr))
+      throw `You must provide an array of ${varName}`;
+    for (let i in arr) {
+      if (typeof arr[i] !== "string" || arr[i].trim().length === 0) {
+        throw `One or more elements in ${varName} array is not a string or is an empty string`;
+      }
+      arr[i] = arr[i].trim();
+    }
+    return arr;
+  },
+
+  checkStringObject(obj, varName) {
+    if (!obj || typeof obj !== "object")
+      throw `You must provide an object of ${varName}`;
+    if (Array.isArray(eventLocation))
+      throw `${varName} must be an object, but an array was supplied`;
+    for (let i in arr) {
+      if (typeof obj[i] !== "string" || obj[i].trim().length === 0) {
+        throw `One or more elements in ${varName} object is not a string or is an empty string`;
+      }
+      obj[i] = obj[i].trim();
+    }
+    return obj;
+  },
+
+  checkIsPositiveNum(val, varName) {
+    if (!val) throw `Error: You must supply a ${varName}!`;
+    if (typeof val !== "number" || val < 0) {
+      throw `${varName || "provided variable"} needs to be a positive integer`;
+    }
+    if (isNaN(val)) {
+      throw `${varName || "provided variable"} is NaN`;
+    }
+    return val;
+  },
+};
+
+export default helperFunctions;
