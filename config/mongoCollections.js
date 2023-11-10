78b9ab68086310bce9d38cfb7b32d7542170b653
Christine Anthony <2christineanthony@gmail.com>
2023-11-10 13:20:26 -0500

    createUser() in data

--- config/mongoCollections.js
+++ config/mongoCollections.js
@@ -1,17 +1,16 @@
-// import { dbConnection } from "./mongoConnection.js";
+import { dbConnection } from "./mongoConnection.js";
 
-// const getCollectionFn = (collection) => {
-//   let _col = undefined;
+const getCollectionFn = (collection) => {
+  let _col = undefined;
 
-//   return async () => {
-//     if (!_col) {
-//       const db = await dbConnection();
-//       _col = await db.collection(collection);
-//     }
+  return async () => {
+    if (!_col) {
+      const db = await dbConnection();
+      _col = await db.collection(collection);
+    }
 
-//     return _col;
-//   };
-// };
+    return _col;
+  };
+};
 
-// // NOTE: YOU WILL NEED TO CHANGE THE CODE BELOW TO HAVE THE COLLECTION(S) REQUIRED BY THE ASSIGNMENT
-// export const events = getCollectionFn("events");
+export const events = getCollectionFn("users");
