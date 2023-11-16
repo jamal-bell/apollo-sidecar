ac38c76817f1c6fbbcb0e5170576362babe6f0ac
jamal-bell <jbell1@stevens.edu>
2023-11-16 03:28:44 -0500

    updated file structure

--- app.js
+++ app.js
@@ -1,12 +1,38 @@
 import express from "express";
-
 const app = express();
+import configRoutes from "./routes/index.js";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import exphbs from "express-handlebars";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-import configRoutFunction from "./routes/index.js";
+const staticDir = express.static(__dirname + "/public");
 
-configRoutFunction(app);
+const rewriteUnsupportedBrowserMethods = (req, res, next) => {
+  // If the user posts to the server with a property called _method, rewrite the request's method
+  // To be that method; so if they post _method=PUT you can now allow browsers to POST to a route that gets
+  // rewritten in this middleware to a PUT route
+  if (req.body && req.body._method) {
+    req.method = req.body._method;
+    delete req.body._method;
+  }
+
+  // let the next middleware run:
+  next();
+};
+
+app.use("/public", staticDir);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(rewriteUnsupportedBrowserMethods);
+
+app.engine("handlebars", exphbs.engine({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+configRoutes(app);
 
 app.listen(3000, () => {
-  console.log("We've got a server!");
-  console.log("Our routes are running on http://localhost:3000");
+  console.log("We've now got a server!");
+  console.log("Your routes will be running on http://localhost:3000");
 });
---
+++ tasks/seed.js
@@ -0,0 +1,37 @@
+import { MongoExpiredSessionError } from "mongodb";
+import { dbConnection, closeConnection } from "./config/mongoConnection.js";
+import userData from "./data/users.js";
+import qaData from "./data/qa.js";
+import lessonData from "./lessons.js"
+
+let newUser = undefined;
+let newlesson = undefined;
+let newQaPost = undefined;
+let newQaResponse = undefined;
+
+// Example to loop and seed database
+
+// try {
+//   for (let i = 0; i < 20; i++) {
+//   newUser = await userData.create(
+//     "Patrick's" + i + "Big End of Summer BBQ",
+//     "Come join us for our yearly end of summer bbq!",
+//     { streetAddress: "1 Castle Point Terrace", city: "Hoboken", state: "NJ", zip: "07030" },
+//     "phillip123@stevens.edu",
+//     30,
+//     0,
+//     "08/25/2024",
+//     "10:00 PM",
+//     "11:10 PM",
+//     false
+//   );
+// }
+//   console.log(newUser);
+// } catch (e) {
+//   console.log("Got an error!");
+//   console.log(e);
+// }
+
+// ADD for a new QA post
+
+// Add for new QA responses
