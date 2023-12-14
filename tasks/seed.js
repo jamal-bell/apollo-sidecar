98f865e3b865b1b5994b45dc9e785ed63d95de45
AstronomicalWood <AstronomicalWood@yahoo.com>
2023-12-14 17:11:40 -0600

    Seed update

--- tasks/seed.js
+++ tasks/seed.js
@@ -2,6 +2,7 @@ import { MongoExpiredSessionError } from 'mongodb';
 import { dbConnection, closeConnection } from '../config/mongoConnection.js';
 import { users, lessons, qa } from '../config/mongoCollections.js';
 import userData from '../data/users.js';
+import { ObjectId } from 'mongodb';
 import qaData from '../data/qa.js';
 import lessonsData from '../data/lessons.js';
 const db = await dbConnection();
@@ -11,255 +12,167 @@ let newLesson = undefined;
 let newQaPost = undefined;
 let newQaResponse = undefined;
 
-// Example user registration calls with random data and strong passwords
 try {
-  await userData.registerUser(
-    'Alice',
-    'Johnson',
-    'alice.j@example.com',
-    'Password123!',
-    'user'
-  );
-  await userData.registerUser(
-    'Bob',
-    'Smith',
-    'bob.smith@example.com',
-    'Secure789!',
-    'user'
-  );
-  await userData.registerUser(
-    'Charlie',
-    'Brown',
-    'charlie.b@example.com',
-    'StrongPass1!',
-    'user'
-  );
-  await userData.registerUser(
-    'David',
-    'Miller',
-    'david.m@example.com',
-    'SafePassword42#',
-    'user'
-  );
-  await userData.registerUser(
-    'Emma',
-    'Taylor',
-    'emma.t@example.com',
-    'Secret123@',
-    'user'
-  );
-  await userData.registerUser(
-    'Frank',
-    'Wilson',
-    'frank.w@example.com',
-    'Hidden567$',
-    'user'
-  );
-  await userData.registerUser(
-    'Grace',
-    'Anderson',
-    'grace.a@example.com',
-    'Protected1@',
-    'user'
-  );
-  await userData.registerUser(
-    'Henry',
-    'Clark',
-    'henry.c@example.com',
-    'Guarded987#',
-    'user'
-  );
-  await userData.registerUser(
-    'Ivy',
-    'Robinson',
-    'ivy.r@example.com',
-    'Locked654!',
-    'user'
-  );
-  await userData.registerUser(
-    'Jack',
-    'Moore',
-    'jack.m@example.com',
-    'Shielded123$',
-    'admin'
-  );
-  await userData.registerUser(
-    'Karen',
-    'Lee',
-    'karen.lee@example.com',
-    'SecretPass12!',
-    'admin'
-  );
-  await userData.registerUser(
-    'Liam',
-    'Garcia',
-    'liam.g@example.com',
-    'SecureCode123!',
-    'user'
-  );
-  await userData.registerUser(
-    'Mia',
-    'Wright',
-    'mia.w@example.com',
-    'HiddenKey789!',
-    'user'
-  );
-  await userData.registerUser(
-    'Noah',
-    'Fisher',
-    'noah.f@example.com',
-    'Guardian789!',
-    'user'
-  );
-  await userData.registerUser(
-    'Olivia',
-    'Baker',
-    'olivia.b@example.com',
-    'SafeLock1@',
-    'user'
-  );
-  await userData.registerUser(
-    'Peter',
-    'King',
-    'peter.k@example.com',
-    'Defender456!',
-    'user'
-  );
-  await userData.registerUser(
-    'Quinn',
-    'Evans',
-    'quinn.e@example.com',
-    'SecureWord1!',
-    'user'
-  );
-  await userData.registerUser(
-    'Rachel',
-    'Young',
-    'rachel.y@example.com',
-    'ProtectedABC!',
-    'user'
-  );
-  await userData.registerUser(
-    'Sam',
-    'Hill',
-    'sam.h@example.com',
-    'GuardedXYZ1@',
-    'admin'
-  );
-  await userData.registerUser(
-    'Tom',
-    'Fisher',
-    'tom.f@example.com',
-    'SecurePass123@',
-    'user'
-  );
-  await userData.registerUser(
-    'Mario',
-    'Plumber',
-    'mario.plumber@example.com',
-    'Luigi123!',
-    'user'
-  );
-  await userData.registerUser(
-    'Luigi',
-    'Green',
-    'luigi.green@example.com',
-    'Mario456!',
-    'user'
-  );
-  await userData.registerUser(
-    'Princess',
-    'Peach',
-    'princess.peach@example.com',
-    'Toadstool789!',
-    'user'
-  );
-  await userData.registerUser(
-    'Bowser',
-    'King',
-    'bowser.king@example.com',
-    'Koopa1234!',
-    'user'
-  );
-  await userData.registerUser(
-    'Yoshi',
-    'Dino',
-    'yoshi.dino@example.com',
-    'EggEater567!',
-    'user'
-  );
-  await userData.registerUser(
-    'Wario',
-    'Greedy',
-    'wario.greedy@example.com',
-    'GoldCoins789!',
-    'user'
-  );
-  await userData.registerUser(
-    'Toad',
-    'Mushroom',
-    'toad.mushroom@example.com',
-    'SporeGuard1@',
-    'user'
-  );
-  await userData.registerUser(
-    'Koopa',
-    'Troopa',
-    'koopa.troopa@example.com',
-    'ShellShield456!',
-    'user'
-  );
-  await userData.registerUser(
-    'Donkey',
-    'Kong',
-    'donkey.kong@example.com',
-    'BananaGuard1@',
-    'admin'
-  );
-  await userData.registerUser(
-    'Princess',
-    'Daisy',
-    'princess.daisy@example.com',
-    'FlowerPower123@',
-    'user'
-  );
+  await Promise.all([
+    userData.registerUser(
+      'Alice',
+      'Johnson',
+      'alice.j@example.com',
+      'Password123!',
+      'user'
+    ),
+    userData.registerUser(
+      'Bob',
+      'Smith',
+      'bob.smith@example.com',
+      'Secure789!',
+      'user'
+    ),
+    userData.registerUser(
+      'Charlie',
+      'Brown',
+      'charlie.b@example.com',
+      'StrongPass1!',
+      'user'
+    ),
+    userData.registerUser(
+      'David',
+      'Miller',
+      'david.m@example.com',
+      'SafePassword42#',
+      'user'
+    ),
+    userData.registerUser(
+      'Sam',
+      'Hill',
+      'sam.h@example.com',
+      'GuardedXYZ1@',
+      'admin'
+    ),
+    userData.registerUser(
+      'Tom',
+      'Fisher',
+      'tom.f@example.com',
+      'SecurePass123@',
+      'user'
+    ),
+    userData.registerUser(
+      'Mario',
+      'Plumber',
+      'mario.plumber@example.com',
+      'Luigi123!',
+      'user'
+    ),
+    userData.registerUser(
+      'Luigi',
+      'Green',
+      'luigi.green@example.com',
+      'Mario456!',
+      'user'
+    ),
+    userData.registerUser(
+      'Princess',
+      'Peach',
+      'princess.peach@example.com',
+      'Toadstool789!',
+      'user'
+    ),
+    userData.registerUser(
+      'Bowser',
+      'King',
+      'bowser.king@example.com',
+      'Koopa1234!',
+      'user'
+    ),
+    userData.registerUser(
+      'Yoshi',
+      'Dino',
+      'yoshi.dino@example.com',
+      'EggEater567!',
+      'user'
+    ),
+    userData.registerUser(
+      'Wario',
+      'Greedy',
+      'wario.greedy@example.com',
+      'GoldCoins789!',
+      'user'
+    ),
+    userData.registerUser(
+      'Toad',
+      'Mushroom',
+      'toad.mushroom@example.com',
+      'SporeGuard1@',
+      'user'
+    ),
+    userData.registerUser(
+      'Koopa',
+      'Troopa',
+      'koopa.troopa@example.com',
+      'ShellShield456!',
+      'user'
+    ),
+    userData.registerUser(
+      'Donkey',
+      'Kong',
+      'donkey.kong@example.com',
+      'BananaGuard1@',
+      'admin'
+    ),
+    userData.registerUser(
+      'Princess',
+      'Daisy',
+      'princess.daisy@example.com',
+      'FlowerPower123@',
+      'user'
+    ),
+  ]);
 } catch (e) {
-  throw new Error('Seeding failed');
+  throw new Error(`Seeding failed at users: ${e.message}`);
 }
-
-const userIds = await userCollection
-  .find()
-  .map((user) => user._id)
-  .toArray();
-
-const lessonsCollection = await lessons();
-
-const lessonTitlePrefix = 'Lesson'; // You can adjust this as needed
-
-const newLessonInfos = Array.from({ length: 30 }, (_, index) => {
-  const randomUserId = userIds[Math.floor(Math.random() * userIds.length)];
-
-  return {
-    lessonTitle: `${lessonTitlePrefix} ${index + 1}`, // Add a lesson index to the title
-    description: `Description for Lesson ${index + 1}`, // Example description
-    creatorId: randomUserId,
-    contents: [
-      {
-        _id: new ObjectId(),
-        order: 1,
-        moduleTitle: `Module for Lesson ${index + 1}`, // Example module title
-        creatorId: randomUserId,
-        author: null,
-        text: `Text for Lesson ${index + 1}`, // Example text
-        videoLink: ['https://example.com/video'], // Example video link
-        createdByRole: '',
-      },
-    ],
-  };
-});
-
-await lessonsCollection.insertMany(newLessonInfos);
-
-console.log(newLessonInfos);
-
-await closeConnection();
+console.log('Seeding users completed!');
+let userIds;
+try {
+  const userCollection = await users();
+  userIds = await userCollection
+    .find()
+    .map((user) => user._id)
+    .toArray();
+} catch (e) {
+  throw new Error(`Flattening CreatorIds failed: ${e.message}`);
+}
+console.log('Flattening IDs for random lesson creation completed!');
+try {
+  const lessonsCollection = await lessons();
+  const lessonTitlePrefix = 'Lesson';
+  await Promise.all(
+    Array.from({ length: 30 }, (_, index) => {
+      const randomUserId = userIds[Math.floor(Math.random() * userIds.length)];
+      const authorChosen = userCollection.findOne({ _id: randomUserId });
+      const newLessonInfo = {
+        lessonTitle: `${lessonTitlePrefix} ${index + 1}`,
+        description: `Description for Lesson ${index + 1}`,
+        creatorId: authorChosen._id,
+        contents: [
+          {
+            _id: new ObjectId(),
+            order: 1,
+            moduleTitle: `Module for Lesson ${index + 1}`,
+            creatorId: authorChosen._id,
+            author: authorChosen.handle,
+            text: `Text for Lesson ${index + 1}`,
+            videoLink: ['https://example.com/video'],
+            createdByRole: authorChosen.role,
+          },
+        ],
+      };
+      return lessonsCollection.insertOne(newLessonInfo);
+    })
+  );
+  console.log('Seeding Lessons Completed!');
+  console.log('Seeding completed successfully!');
+} catch (e) {
+  console.error('Seeding failed at lessons:', e);
+} finally {
+  await closeConnection();
+}
